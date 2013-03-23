6780ead21d46c482468240f541f86c58637ddf13
Александр Комаров <akomarov@e8704808-1a2a-453a-8987-66899a109cbe>
2013-03-23 20:53:30 +0000

    все еще путаюсь как коммитить так, чтобы новые файлы добавлялись сразу =/
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@11 e8704808-1a2a-453a-8987-66899a109cbe

---
+++ GL/Models/Auth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GL.Models
+{
+    public class Auth
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Вы не ввели логин")]
+        [Display(Name = "Логин")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Вы не ввели пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
