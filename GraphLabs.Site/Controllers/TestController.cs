ef1d019678363b58d544e271e18ef6c1701bfb6e
Александр Комаров <akomarov@e8704808-1a2a-453a-8987-66899a109cbe>
2013-05-24 21:07:55 +0000

    "мусорная" ветка развития, в которой я сделал на jQuery страничку с 2 списками и возможностью добавлять элементы из первого списка во второй (наработка навыков перед реализацией задачи с составлением контрольной) добавил, чтобы видно было что работаю (изучаю js) + не потерять заготовки скриптов для страницы
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@45 e8704808-1a2a-453a-8987-66899a109cbe

---
+++ GraphLabs.Site/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GraphLabs.DomainModel;
+using GraphLabs.Site.Utils;
+
+namespace GraphLabs.Site.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly GraphLabsContext _ctx = new GraphLabsContext();
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            this.AllowAnonymous(_ctx);
+            return View();
+        }
+
+    }
+}
