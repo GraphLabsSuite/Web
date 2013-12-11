939646e060df1af22d2f7fcc95250ee0c07f342a
Святослав Захряпин <svt.zhr@gmail.com>
2013-12-11 21:06:48 +0000

    bug 0 EF6!
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@89 e8704808-1a2a-453a-8987-66899a109cbe

--- GraphLabs.Site/Controllers/LabsController.cs
+++ GraphLabs.Site/Controllers/LabsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
--- GraphLabs.Site/Controllers/UserController.cs
+++ GraphLabs.Site/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
