1b6abf5899b4c90c4d76b967bb7b2a3eace3e23c
Александр Комаров <akomarov@e8704808-1a2a-453a-8987-66899a109cbe>
2014-03-20 15:02:18 +0000

    Первая функциональность страницы результатов студентов для преподавателей
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@108 e8704808-1a2a-453a-8987-66899a109cbe

---
+++ GraphLabs.Site/Controllers/ResultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GraphLabs.DomainModel;
+using GraphLabs.Site.Models;
+using GraphLabs.Site.Utils;
+using Newtonsoft.Json;
+
+namespace GraphLabs.Site.Controllers
+{
+    public class ResultController : Controller
+    {
+        private readonly GraphLabsContext _ctx = new GraphLabsContext();
+
+        public ActionResult Index()
+        {
+            this.AllowAnonymous(_ctx);
+
+            var groups = (from g in _ctx.Groups
+                          select g).ToArray()
+                          .Select(t => new GroupModel(t))
+                          .ToArray();
+
+            return View(groups);
+        }
+
+        [HttpPost]
+        public string GetGroupsInfo(string Groups)
+        {
+            Groups = "[" + Groups + "]";
+            List<long> groupsId = JsonConvert.DeserializeObject<List<long>>(Groups);
+
+            JSONResultGroupsInfoModel result = new JSONResultGroupsInfoModel();
+            result.Result = 0;
+            result.Marks = new GroupResult[groupsId.Count];
+
+            GroupModel group;
+            int j = 0;
+
+            foreach (long i in groupsId)
+            {
+                group = new GroupModel(_ctx.Groups.Find(i));
+                result.Marks[j] = new GroupResult();
+                result.Marks[j].Id = group.Id;
+                result.Marks[j].Name = group.Name;
+                result.Marks[j].StudentsCount = group.Students.Count;
+                result.Marks[j].Count5 = 5;
+                result.Marks[j].Count4 = 6;
+                result.Marks[j].Count3 = 7;
+                result.Marks[j].Count2 = 8;
+                result.Marks[j].Count0 = 2;
+                ++j;
+            }
+
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
---
+++ GraphLabs.Site/Models/JSONResultGroupsInfoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphLabs.Site.Models
+{
+    public class GroupResult
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int StudentsCount { get; set; }
+
+        public int Count5 { get; set; }
+
+        public int Count4 { get; set; }
+
+        public int Count3 { get; set; }
+
+        public int Count2 { get; set; }
+
+        public int Count0 { get; set; }
+    }
+
+    public class JSONResultGroupsInfoModel
+    {
+        public int Result { get; set; }
+
+        public GroupResult[] Marks { get; set; }
+    }
+}
