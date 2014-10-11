687e00134899a4655b20bcb4f244215caf11ffee
Сергей Горюшкин <SGorushkin@e8704808-1a2a-453a-8987-66899a109cbe>
2014-10-11 06:05:36 +0000

    Bug 120
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@192 e8704808-1a2a-453a-8987-66899a109cbe

---
+++ GraphLabs.Site/Controllers/SurveyController.cs
@@ -0,0 +1,30 @@
+﻿using GraphLabs.DomainModel;
+using GraphLabs.DomainModel.Repositories;
+using GraphLabs.Site.Controllers.Attributes;
+using GraphLabs.Site.Controllers.LabWorks;
+using GraphLabs.Site.Logic.LabsLogic;
+using GraphLabs.Site.Models;
+using GraphLabs.Site.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphLabs.Site.Controllers
+{
+    [GLAuthorize(UserRole.Administrator, UserRole.Teacher)]
+    public class SurveyController : GraphLabsController
+    {
+
+        public ActionResult Create ()
+        {
+            var result = new SurveyCreatingModel();
+            result.questionOptions = new List<KeyValuePair<string, bool>>();
+            result.questionOptions.Add(new KeyValuePair<string, bool> ("2131231xdcfsdrevf", false));
+            return View(result);
+        }
+
+
+    }
+}
---
+++ GraphLabs.Site/Models/SurveyCreatingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphLabs.Site.Models
+{
+    public class SurveyCreatingModel
+    {
+        public string question { get; set; }
+        public List<KeyValuePair<string, bool>> questionOptions { get; set; }
+    }
+}
