b10d859077b65f8e5fba65dac2d0e9a30b7c4916
Святослав Захряпин <svt.zhr@gmail.com>
2014-11-08 08:52:31 +0000

    WorkingMode
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@213 e8704808-1a2a-453a-8987-66899a109cbe

---
+++ GraphLabs.Utils/WorkingMode.cs
@@ -0,0 +1,15 @@
+using System.Web.Configuration;
+
+namespace GraphLabs.Utils
+{
+    /// <summary> Режим работы приложения </summary>
+    public static class WorkingMode
+    {
+        /// <summary> Debug? </summary>
+        public static bool IsDebug()
+        {
+            bool result;
+            return bool.TryParse(WebConfigurationManager.AppSettings["IsDebug"], out result) && result;
+        }
+    }
+}
