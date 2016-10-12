d2ca04bf0c85043e5c92f710ba74ec18372fb4fe
Святослав Захряпин <svt.zhr@gmail.com>
2016-10-12 20:09:38 +0000

    Reverse Merged revision(s) 353 from trunk: сущность-расписание лаб\ ........
    git-svn-id: https://svtz.ru/svn/graphlabs.site/trunk@354 e8704808-1a2a-453a-8987-66899a109cbe

--- GraphLabs.Dal.Ef/GraphLabsDataModel.Designer.cs
+++ GraphLabs.Dal.Ef/GraphLabsDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\sozak\Desktop\GraphLabs\Site\GraphLabs.Dal.Ef\GraphLabsDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Егор\Desktop\graphlabs.site\trunk\GraphLabs.Dal.Ef\GraphLabsDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
