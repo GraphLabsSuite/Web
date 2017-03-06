b99f181fb2de982a2d92da62e068095e8772bb80
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2017-03-07 02:03:55 +0300

    скрипт генерации номера версии из git

--- GraphLabs.Tests.Site.Logic/Properties/AssemblyInfo.cs
+++ GraphLabs.Tests.Site.Logic/Properties/AssemblyInfo.cs
@@ -8,23 +8,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ba78726-95ff-4f48-8f5d-44fcb9bb19a4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
---
+++ generateVersionInfo.ps1
@@ -0,0 +1,50 @@
+﻿$debugVersion = ""
+$version = ""
+
+$hasChanges = $false
+$diff = git diff-index HEAD
+if ($diff) {
+    $hasChanges = $true
+}
+
+$versionString = Get-Content ".\version.txt"
+$versionSegments = $versionString.Split("{.}")
+if ($versionSegments.Length -ne 3)
+{
+    Throw "Файл version.txt должен содержать номер версии в формате X.Y.Z"
+}
+
+$currentBranch = git rev-parse --abbrev-ref HEAD
+$commitsCount = git rev-list --count HEAD
+if (!$hasChanges -and ($currentBranch -eq "master")) {
+    $version = "$($versionSegments[0]).$($versionSegments[1]).$($versionSegments[2])"
+    $debugVersion = "$version.$commitsCount"
+}
+else {
+    $version = "$($versionSegments[0]).$($versionSegments[1]).$($versionSegments[2]).$($commitsCount + 1)"
+    $debugVersion = $version
+}
+
+$versionInfo = @"
+/* 
+    Этот файл генерируется автоматически при сборке проекта.
+    Номер версии задаётся в "version.txt" в формате X.Y.Z
+   
+    "Корректная" версия из 3х цифр (SEMVER) прописывается на dll'ки, только если выполнены условия:
+    * в рабочей копии отсутствуют незафиксированные изменения
+    * сборка производится из ветки master
+    * сборка производится в конфигурации Release
+*/
+
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyVersion("$debugVersion")]
+[assembly: AssemblyInformationalVersion("$debugVersion")]
+#else
+[assembly: AssemblyVersion("$version")]
+[assembly: AssemblyInformationalVersion("$version")]
+#endif
+"@
+
+Set-Content -Encoding UTF8 -Path "AssemblyVersion.cs" -Value $versionInfo
