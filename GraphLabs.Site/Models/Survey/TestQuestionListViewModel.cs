02eaa51be051c784af40b76d74c513fb41bf6034
elka <rock-paper-scissors@yandex.ru>
2018-03-09 09:32:52 +0300

    Revert "модели для syrvey перенесены в GraphLabs.Site.Models"
    This reverts commit 4aabdefd0fe1273bf8684b8bfd4d56ebf0bcf87f.

---
+++ GraphLabs.Site/Models/Survey/TestQuestionListViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GraphLabs.DomainModel.Repositories;
+
+namespace GraphLabs.Site.Models
+{
+    public class TestQuestionListViewModel : BaseViewModel
+    {
+        #region Зависимости
+
+        private readonly ISurveyRepository _surveyRepository;
+
+        #endregion
+
+		public bool ShowCategory { get; set; }
+
+		public List<TestQuestionDto> Items { get; private set; }
+
+        public void Load(long CategoryId)
+        {
+            ShowCategory = CategoryId == 0;
+
+            var questions = CategoryId == 0
+                ? _surveyRepository.GetAllQuestions()
+                : _surveyRepository.GetQuestionByCategory(CategoryId);
+
+            Items = questions.Select(q => new TestQuestionDto
+            {
+                QuestionId = q.Id,
+                Question = q.Question,
+                QuestionCategory = q.Category.Name,
+                QuestionSubCategory = q.SubCategory.Name
+            })
+                .ToList();
+        }
+		public TestQuestionListViewModel(ISurveyRepository surveyRepository)
+		{
+		    _surveyRepository = surveyRepository;
+		}
+    }
+
+	public class TestQuestionDto
+	{
+		public long QuestionId { get; set; }
+		public string Question { get; set; }
+		public string QuestionCategory { get; set; }
+        public string QuestionSubCategory { get; set; }
+    }
+}
