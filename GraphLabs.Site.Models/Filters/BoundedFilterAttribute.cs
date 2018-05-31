8142b1cf54c4a4fe1a0d9a2d30c238d490bc0656
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-05-31 07:40:37 +0300

    есть ли билд после мержда?

--- GraphLabs.Site.Models/Filters/BoundedFilterAttribute.cs
+++ GraphLabs.Site.Models/Filters/BoundedFilterAttribute.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 
 namespace GraphLabs.Site.Core.Filters
 {
-    [System.AttributeUsage(System.AttributeTargets.Property)]  
+    [AttributeUsage(AttributeTargets.Property)]  
     public class BoundedFilterAttribute : Attribute, IFilterAttribute
     {
-        private String _name;
-        private Object[] _limiters;
+        private string _name;
+        private object[] _limiters;
       
-        public BoundedFilterAttribute(string name, Object[] limiters)
+        public BoundedFilterAttribute(string name, object[] limiters)
         {
             _name = name;
             _limiters = limiters; 
